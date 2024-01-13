2434635b2b178ea4fbf110781d6a371ecff141c6
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2024-01-13 22:13:20 +0100

    roba

--- FIRST2D/main.gd
+++ FIRST2D/main.gd
@@ -5,7 +5,8 @@ extends Node
 var score
 
 # Called when the node enters the scene tree for the first time.
-func _ready(): new_game()
+func _ready(): 
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,12 +17,17 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
+	get_tree().call_group("mobs", "queue_free")
 	score=0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_messages("DAI SU COGGHIONE")
+	
 
 
 func _on_mob_timer_timeout():
@@ -53,6 +59,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
