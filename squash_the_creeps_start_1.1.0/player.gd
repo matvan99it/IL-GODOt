dfcb5b69b4af3cfe1238a035e0cf9f3c8e2b87b3
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-02-15 18:46:56 +0100

    Animazioni pg

--- squash_the_creeps_start_1.1.0/player.gd
+++ squash_the_creeps_start_1.1.0/player.gd
@@ -32,6 +32,9 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
+		$AnimationPlayer.speed_scale = 4
+	else:
+		$AnimationPlayer.speed_scale = 1
 
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
@@ -64,6 +67,7 @@ func _physics_process(delta):
 				
 				break # Prevent furter duplicate calls
 	
+	$Pivot.rotation.x = PI / 6 * velocity.y / jump_impulse
 	
 	
 	move_and_slide()
