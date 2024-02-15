08b505d20ac7d42c71589302428b8a8d3fb02c99
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-02-15 17:37:45 +0100

    Aggiunto il jump ed eliminazione mobbini

--- squash_the_creeps_start_1.1.0/player.gd
+++ squash_the_creeps_start_1.1.0/player.gd
@@ -6,28 +6,27 @@ extends CharacterBody3D
 @export var fall_acceleration = 75
 # Vertical impulse applied to the character upon jumping in meters per second.
 @export var jump_impulse = 20
+# Vertical impulse applied to the character upon bouncing over a mob in
+# meters per second.
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
+
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
-		print("RIGHT")
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-		print("LEFT")
 	if Input.is_action_pressed("move_back"):
 		direction.z += 1
-		print("UP")
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
-		print("DOWN")
-	# Jumping.
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		target_velocity.y = jump_impulse
-
+		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
@@ -42,4 +41,27 @@ func _physics_process(delta):
 
 	# Moving the Character
 	velocity = target_velocity
+	
+	#Iteretion through all collision happening
+	for index in range(get_slide_collision_count()):
+		#The one with the player
+		var collision = get_slide_collision(index)
+		
+		if collision.get_collider() == null: #Ground
+			continue
+		
+		if collision.get_collider().is_in_group("mob"): #Collision with other mobs
+			var mob = collision.get_collider()
+			
+			#Check hit from above
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				
+				#Squash and bounce
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				
+				break # Prevent furter duplicate calls
+	
+	
+	
 	move_and_slide()
