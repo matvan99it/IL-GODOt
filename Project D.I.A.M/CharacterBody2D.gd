c4ca9a4414c89fe140be6cd004d372e9dc4088cb
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2024-09-13 17:52:32 +0200

    comandi alla deadcell quasi ultimati

--- Project D.I.A.M/CharacterBody2D.gd
+++ Project D.I.A.M/CharacterBody2D.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
+const SLIDE_SPEED = 500.0
+const GO_DOWN = 1000.0
+const DASH_DURATION = 0.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var tween: Tween
+var dash_velocity := 0.0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,13 +20,28 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if(Input.is_action_just_pressed("ui_down") and not is_on_floor()):
+		velocity.y = GO_DOWN
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if Input.is_action_just_pressed("dash"):
+		dash_velocity = SLIDE_SPEED
+		rotate(deg_to_rad(90))
+		if tween:
+			tween.stop()
+			rotate(deg_to_rad(0))
+		tween = create_tween()
+		tween.tween_property(self, "dash_velocity", 0, 0.3).set_ease(Tween.EASE_OUT)
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * (SPEED + dash_velocity)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	
 
 	move_and_slide()
