1da39a2c640701b26a3bb6feb712a0d593603ebf
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2024-09-15 17:35:16 +0200

    la rotatoria animata

--- Project D.I.A.M/main.gd
+++ Project D.I.A.M/main.gd
@@ -4,5 +4,5 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$sliderAnimation.play("slide")
-	#$circleAnimation.play("rotatoria_pazza")
+	$circleAnimation.play("rotatoria_pazza")
 
