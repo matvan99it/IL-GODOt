da22254286a54529e513afd987daa9dee4010cc4
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-09-16 16:09:41 +0200

    cgso voice sez: enemy spawned

---
+++ Project D.I.A.M/enemy.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
