938337e1bc4c1658a685cb189ece379524c5205e
MatVan99ita <56399084+MatVan99ita@users.noreply.github.com>
2024-09-17 15:51:06 +0200

    aggiunti todo

---
+++ Project D.I.A.M/sesso_pazzo.gd
@@ -0,0 +1,15 @@
+extends Node
+
+#TODO: implementare combat system
+#TODO: aggiungere shader lampeggiante per il danno inferto e subito
+#TODO: aggiungere una mini pausa per i danni presi e subiti e incrementale in base al tipo di danno
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
