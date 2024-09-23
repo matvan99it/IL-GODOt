2c0b7c3ee412972d9c8b5123acf67f05a1068bca
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-09-23 15:43:41 +0200

    capito come sessare

--- Project D.I.A.M/scripts/enemy.gd
+++ Project D.I.A.M/scripts/enemy.gd
@@ -1,5 +1,27 @@
 extends CharacterBody2D
 
-#func _on_visible_on_screen_notifier_2d_2_screen_exited():
-#	queue_free()
+const SPEED = 200.0
+var chasing: bool = false
+var player = null
 
+func _physics_process(delta):
+	if chasing and player:
+		var direction = (player.position - position).normalized()
+		velocity = direction * SPEED
+	else:
+		velocity = Vector2.ZERO  # Il nemico si ferma se non insegue il giocatore
+
+	# Muove il nemico e gestisce le collisioni
+	move_and_slide()
+
+func _on_detection_area_body_entered(body):
+	if body.is_in_group("player"): # Assicurati che il rilevamento funzioni solo per il player
+		player = body
+		chasing = true
+		rotate(deg_to_rad(90))
+
+func _on_detection_area_body_exited(body):
+	if body.is_in_group("player"):
+		player = null
+		chasing = false
+		rotate(deg_to_rad(-90))
--- Project D.I.A.M/scripts/main.gd
+++ Project D.I.A.M/scripts/main.gd
@@ -9,23 +9,9 @@ func _ready():
 
 func new_game():
 	get_tree().call_group("mobs", "queue_free")
-	$EnemySpawnTimer.start()
-	if ($EnemySpawnTimer.time_left > 0):
-		print ("Timer is active")
-		$CanvasLayer/LabelTimer.text += "pippo"
-	else:
-		$CanvasLayer/LabelTimer.text += "\n ODIO"
 
 func _on_enemy_spawn_timer_timeout():
-	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
-
-	# Choose a random location on Path2D.
 	var mob_spawn_location = $EnemySpawn
-	# Set the mob's position to a random location.
 	mob.position = mob_spawn_location.position
-	
-
-	
-	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
