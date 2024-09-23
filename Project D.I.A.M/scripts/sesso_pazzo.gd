73fcfaa2eb28fbbbe86c2f678d09fa7a160ae2cf
MatVan99ita <matteo.vanni2@studio.unibo.it>
2024-09-23 16:33:44 +0200

    one way platform
    ma non come piacciono a me

--- Project D.I.A.M/scripts/sesso_pazzo.gd
+++ Project D.I.A.M/scripts/sesso_pazzo.gd
@@ -5,7 +5,7 @@ extends Node
 #TODO: aggiungere shader lampeggiante per il danno inferto e subito in base al tipo di nemico
 #TODO: aggiungere una mini pausa per i danni presi e subiti e incrementale in base al tipo di danno
 #TODO: aggiungere una specie di knockbck per i nemici più piccoli
-
+#TODO: sistemare le piattaforme in modo che possa gestirle anche da codice e migliorare le grafiche e gli allineamenti
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
